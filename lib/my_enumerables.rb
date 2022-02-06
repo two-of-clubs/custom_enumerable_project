ba28016e322d1a35fd7a997fff380f8ff5b32e23
Ariel Leyva <aleyva1509@gmail.com>
2022-02-06 16:52:09 -0600

    solved a couple

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,4 +1,13 @@
 module Enumerable
+  def my_select
+    selected_values = []
+    self.my_each do |v|
+      if yield(v)
+        selected_values << v 
+      end
+    end
+    selected_values
+  end
   # Your code goes here
 end
 
@@ -7,5 +16,22 @@ end
 # your enumerable module will have access
 # to this method
 class Array
+  def my_each
+    (self.size()).times do |i| 
+      yield self[i]
+    end
+    self
+  end
   # Define my_each here
+  def my_each_with_index
+    (self.size()).times do |i|
+      yield(self[i],i)
+    end
+    self
+  end
+
 end
+
+numbers = [1,2,3,4,5]
+puts numbers.select {|v| v>2}
+puts numbers.my_select {|v| v>2}
